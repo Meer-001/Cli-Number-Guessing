711d7d522e0f6f244c62865505ba14001e4123d9
Meer-001 <ahmer.rehman001@gmail.com>
2024-04-22 05:17:46 +0500

    successfully created a number guessing game.

---
+++ main.ts
@@ -0,0 +1,24 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+console.log("Welcome to my Number Guessing Gane, Hope you enjoyed");
+
+
+
+
+const answer = await inquirer.prompt([
+  {
+    name: "userGuessedNumber",
+    type: "number",
+    message: "Enter your guessed number between 1 to 6: ",
+  },
+]);
+if (answer.userGuessedNumber === randomNumber) {
+  console.log("Hurray!! You guessed the number");
+} else {
+  console.log("You guessed the wrong number");
+}
+console.log("Credit: Ahmer Shaikh");
+
